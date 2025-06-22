ad32dac177dac046edc2d4c2b2c584c7bc49727a
aditi saudagar <aditisaudagar17@gmail.com>
2025-06-22 16:26:00 +0530

    document

---
+++ script2.js
@@ -0,0 +1,8 @@
+// change the styles of elements
+
+const mainHeading = document.querySelector("div.headline h2")
+// console.log(mainHeading.style);
+mainHeading.style.color = "blue";
+mainHeading.style.backgroundColor = "white";
+mainHeading.style.border = "solid 2px";
+console.log(mainHeading);
